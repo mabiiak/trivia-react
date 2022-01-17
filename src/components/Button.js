e0648b9e41a63fee0c67b4da41144398e1e3e986
Kazuo Abduch <76076430+kazuo-abduch@users.noreply.github.com>
2022-01-17 19:20:04 -0300

    Muda cor dos botões/respostas

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import './Button.css';
 
 export default class Button extends Component {
   render() {
-    const { onClick, label, dataTest, buttDisabled } = this.props;
+    const { onClick, label, dataTest, buttDisabled, className } = this.props;
     return (
       <button
+        className={ className }
         type="button"
         onClick={ onClick }
         data-testid={ dataTest }
@@ -22,4 +24,5 @@ Button.propTypes = {
   label: PropTypes.string.isRequired,
   dataTest: PropTypes.string.isRequired,
   buttDisabled: PropTypes.bool.isRequired,
+  className: PropTypes.string.isRequired,
 };
