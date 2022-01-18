437075b3442686dff580ed69ecf4f8623171adf6
Kazuo Abduch <76076430+kazuo-abduch@users.noreply.github.com>
2022-01-18 14:52:19 -0300

    Requisito 10 - Inicia implementação do botão Next

--- src/pages/Game.jsx
+++ src/pages/Game.jsx
@@ -12,11 +12,13 @@ class Game extends React.Component {
     this.state = {
       answersList: [],
       buttonDisabled: false,
+      questionIndex: 0,
     };
 
     this.renderAnswers = this.renderAnswers.bind(this);
     this.onClickAnswer = this.onClickAnswer.bind(this);
     this.handleAnswers = this.handleAnswers.bind(this);
+    this.handleNext = this.handleNext.bind(this);
   }
 
   componentDidMount() {
@@ -32,9 +34,10 @@ class Game extends React.Component {
   }
 
   handleAnswers(questionList) {
+    const { questionIndex } = this.state;
     const orderList = [
-      questionList[0].correct_answer,
-      ...questionList[0].incorrect_answers,
+      questionList[questionIndex].correct_answer,
+      ...questionList[questionIndex].incorrect_answers,
     ];
     const RANDOM_INTERVAL = 0.5;
     const shuffledList = orderList.sort(() => Math.random() - RANDOM_INTERVAL);
@@ -45,11 +48,20 @@ class Game extends React.Component {
     });
   }
 
-  renderAnswers() {
+  handleNext() {
+    const ADD_TO_INDEX = 1;
+    const MAX_INDEX = 4;
+    const { questionIndex } = this.state;
+    if (questionIndex < MAX_INDEX) {
+      this.setState({ questionIndex: questionIndex + ADD_TO_INDEX });
+    }
+  }
+
+  renderAnswers(questionIndex) {
     const { questionList } = this.props;
     const { answersList, buttonDisabled } = this.state;
     const { onClickAnswer } = this;
-    const incorrectAnswers = questionList[0].incorrect_answers;
+    const incorrectAnswers = questionList[questionIndex].incorrect_answers;
 
     return (
       answersList.map((answer, index) => {
@@ -80,20 +92,33 @@ class Game extends React.Component {
 
   render() {
     const { questionList } = this.props;
-    const { renderAnswers } = this;
+    const {
+      state: {
+        questionIndex,
+      },
+      renderAnswers,
+      handleNext,
+    } = this;
     return (
       <div>
         <Header />
         <section>
           <div data-testid="question-category">
-            { questionList.length > 0 && `Categoria - ${questionList[0].category} ` }
+            { questionList.length > 0
+            && `Categoria - ${questionList[questionIndex].category} ` }
           </div>
           <div data-testid="question-text">
-            { questionList.length > 0 && questionList[0].question }
+            { questionList.length > 0 && questionList[questionIndex].question }
           </div>
         </section>
         <section data-testid="answer-options">
-          { questionList.length > 0 && renderAnswers() }
+          { questionList.length > 0 && renderAnswers(questionIndex) }
+          <div>
+            <Button
+              label="Next"
+              onClick={ handleNext }
+            />
+          </div>
         </section>
       </div>
     );
