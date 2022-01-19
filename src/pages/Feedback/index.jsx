c3ccd2e88b35543e590c55ec1741836220be8b80
mabiane <mabiane.polniak@gmail.com>
2022-01-19 15:51:09 -0300

    adiciona pontuaçao total a tela de feedback

--- src/pages/Feedback/index.jsx
+++ src/pages/Feedback/index.jsx
@@ -1,15 +1,41 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 import { Header } from '../../components';
 
 class Feedback extends React.Component {
   render() {
+    const { totalScore, assertions, nameUser } = this.props;
     return (
       <div>
         <Header />
-        <div data-testid="feedback-text">Olá</div>
+        <div data-testid="feedback-text">
+          <p data-testeid="feedback-total-question">
+            De 5 questões você acertou
+            { ` &{assertions} ` }
+          </p>
+          <p data-testid="feedback-total-score">
+            Parabéns
+            { ` ${nameUser}` }
+            ! Sua pontuação foi de:
+            {` ${totalScore}`}
+          </p>
+        </div>
       </div>
     );
   }
 }
 
-export default Feedback;
+const mapStateToProps = (state) => ({
+  nameUser: state.player.name,
+  totalScore: state.player.score,
+  assertions: state.player.assertions,
+});
+
+Feedback.propTypes = {
+  totalScore: PropTypes.number.isRequired,
+  assertions: PropTypes.number.isRequired,
+  nameUser: PropTypes.string.isRequired,
+};
+
+export default connect(mapStateToProps, null)(Feedback);
